ae29012a0c1cfa7ea30e93d54b8a4654811e4531
erhanask <erhanasik8@gmail.com>
2023-09-27 21:26:44 +0300

    Added Mediatr package and edited ActivitiesController.cs list and detail functions.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,6 @@
-﻿using Domain;
+﻿using Application.Activities;
+using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -7,23 +9,18 @@ namespace API.Controllers;
 
 public class ActivitiesController: BaseApiController
 {
-    private readonly DataContext _context;
-
-    public ActivitiesController(DataContext context)
-    {
-        _context = context;
-    }
     
     [HttpGet] // api/activities
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await _context.Activities.ToListAsync();
+        return await Mediator.Send(new List.Query());
     }
     
     [HttpGet]
     [Route("{id}")] // api/activities/{id}
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-        return await _context.Activities.FindAsync(id);
+        return await Mediator.Send(new Detail.Query{Id = id});
     }
+    
 }
